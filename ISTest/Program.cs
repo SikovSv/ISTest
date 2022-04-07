0de85aaf8717981167be34be1719224886720fb8
Sikov Sergey <sikovsv@gmail.com>
2022-04-07 14:44:12 +0500

    Добавьте файлы проекта.

---
+++ ISTest/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
