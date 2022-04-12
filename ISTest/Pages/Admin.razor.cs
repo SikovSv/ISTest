405fb81bf17824584133662c450d42b461db9bc3
Sikov Sergey <sikovsv@gmail.com>
2022-04-12 08:01:50 +0500

    Add Admin page

---
+++ ISTest/Pages/Admin.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace ISTest.Pages;
+
+public partial class Admin
+{
+    [Parameter]
+    public string AdminKey { get; set; }
+
+    [Inject]
+    protected IConfiguration Configuration { get; set; }
+
+    protected bool IsAccess { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        IsAccess = Configuration["AdminKey"] == AdminKey;
+    }    
+}
